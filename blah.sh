9f03664074601c3ac503d766c720fdc3d70f7990
Hanynowsky <hanynowsky@gmail.com>
2012-11-30 15:42:55 +0000

    Update blah.sh

--- blah.sh
+++ blah.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 echo "JDebianzier filename is: $0"
-filename=$(echo "$0" | sed -e 's/ /\\ /g' )
-echo "Modified JDebianizer filename is: $filename"
-if [ -f "$filename" ]; then 
-    for i in 10; do
-	echo $i
-bash "$filename"
-    done
-else echo "Path to file is incorrect or contains trailing spaces. (e.g. .../Ubuntu One/....)"
-fi
-
 exit 0;
